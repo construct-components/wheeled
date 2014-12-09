e5e9036fed8105932403ac9484edc670bd733e15
Makis Tracend <makis.tracend@gmail.com>
2014-12-08 22:28:28 -0800

    scaffolding component lib

--- assets/js/construct.component.js
+++ assets/js/construct.component.js
@@ -1,1 +1,13 @@
-/* custom logic goes here */
+/* replace with component lib */
+
+(function( APP ){
+
+	APP = APP || window.APP;
+
+	var View = APP.View.extend({
+
+	});
+
+	APP.Group = APP.Group || {};
+	APP.Group.Component = View;
+})( this.APP );
